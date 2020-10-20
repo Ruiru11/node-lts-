79d969c793ed772ea86f7b4b81e173a3f11abbfc
Erastus Ruiru <erastusruiru11@gmail.com>
2020-10-20 21:54:59 +0300

    ft(env): Get mongo uri from env
    - get mongo uri from env file

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const mongoose = require("mongoose");
 const store = createStore();
 
 // mongo db connection 
-mongoose.connect("mongodb://localhost:27017/nodelts", {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
